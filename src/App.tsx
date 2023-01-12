5917c4887b8cbdeea2a48dca7a15f20327599a02
unknown <awaishk640@gmail.com>
2023-01-12 22:55:26 +0530

    Added a chessboard with a knight at default position

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import "./global.css"
+import knight from "./download.png"
 
 function App() {
+  const [chessBoard, setChessBoard] = useState<Array<number>>([1])
+  const [position, setPosition] = useState<number>(0);
+  let offset = 0;
+  let offsetHelper = 0;
+  useEffect(() => {
+    setChessBoard([1])
+    for(let i = 1; i < 64; i++){
+      setChessBoard((prev) => {
+        return [...prev, prev[prev.length - 1] + 1]
+      })
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='chessBoard'>
+    {
+      chessBoard.map((cell, index) => {
+        if(offsetHelper === 8){
+          offset++
+          offsetHelper = 0
+        }
+        offsetHelper++
+        return <div className={(index + offset) % 2 ? 'whiteCell' : "blackCell"} key={index}>
+          <p className='cellPara'>{cell}</p>
+          {index === position ? <img src={knight} width="40px" className="knightImage" /> : null }</div>
+      })
+    }
     </div>
   );
 }
